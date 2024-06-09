dadc657863e4a612150c7a11e77d7d33ba93a219
Bojan SKI <codeucenje@gmail.com>
2024-06-09 14:41:27 +0200

    Blog page small update

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -7,8 +7,21 @@ const Blog = () => {
       {/* page location */}
       <PageLocation />
 
-      <div className="container">
-        <h1>Blog</h1>      
+      <div className="blog-content">
+        <div className="container text-center py-5">
+          <h1 className="fw-bold mb-3">
+            Hvala Vam što ste pokazali interesovanje za našu Blog stranicu
+          </h1>
+          <h2 className="text-muted mb-3">
+            Nažalost stranica je i dalje u fazi izrade. 
+          </h2>
+          <h2 className="text-muted mb-3">
+            Molimo Vas dođite nam kasnije
+          </h2>
+          <h3 className="fw-bold">
+            S poštovanjem Vaš Portal JEFTINA KUĆA
+          </h3>
+        </div>
       </div>
     </div>
   )
